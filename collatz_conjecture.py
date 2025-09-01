af97ae97603cc61e679dfdd8465e141498f151fb
Siam <siam48444@gmail.com>
2025-09-01 15:34:14 +0600

    siam

---
+++ collatz_conjecture.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Custom fonts
+plt.rcParams['font.family'] = 'KaTeX_Main'
+plt.rcParams['mathtext.fontset'] = 'cm'
+
+
+
+def main():
+    pass
+
+    
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
