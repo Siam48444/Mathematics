94d716396074805256fb20c99b84c5692a06e348
Siam <siam48444@gmail.com>
2025-09-01 17:17:57 +0600

    siam

--- collatz_conjecture.py
+++ collatz_conjecture.py
@@ -18,6 +18,7 @@ ax.grid(True, color="lightgray", linestyle="--", linewidth=0.7)
 def main():
     # plot_sequence([703])
     # print(max_in_range(1, 1000))
+    plot_stopping_time([i for i in range(1, 1000+1)])
 
 
 
@@ -86,6 +87,23 @@ def plot_sequence(numbers: list[int]) -> None:
 
     plt.show()
 
+
+
+def plot_stopping_time(numbers: list[int]) -> None:
+    if any(not isinstance(n, int) or n <= 0 for n in numbers):
+        print("Invalid input list. Must contain positive integers.")
+        return
+
+    for num in numbers:
+        x = num
+        y = collatz_stopping_time(num)
+        ax.plot(x, y, marker="o", markersize=5, linestyle="", color="red")
+
+    ax.set_title("Collatz Stopping Times", fontsize=title_fontsize)
+    ax.set_xlabel("Starting Number", fontsize=label_fontsize)
+    ax.set_ylabel("Stopping Time", fontsize=label_fontsize)
+
+    plt.show()
 
 
 
