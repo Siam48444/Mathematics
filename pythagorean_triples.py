4943aa580c5d1b7a0897e459d28f9fbba9a02319
Siam <siam48444@gmail.com>
2025-09-09 18:13:35 +0600

    siam

--- pythagorean_triples.py
+++ pythagorean_triples.py
@@ -1,12 +1,12 @@
-def main():
-    lim = 5
+def main() -> None:
+    lim = 5000
     for i in range(1, lim):
         for j in range(i+1, lim+1):
-            print(P(i, j))
+            print(pythagorean_triples(i, j))
 
 
 
-def P(a: int, b: int) -> tuple[int, int, int]:
+def pythagorean_triples(a: int, b: int) -> tuple[int, int, int]:
     return (abs(a**2 - b**2), 2*a*b, a**2 + b**2)
 
 
